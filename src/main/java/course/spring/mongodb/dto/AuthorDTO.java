ed03022841440c25440dc356e9da948746d8fad3
oLucasNascimento <lucas.nferreira@bandtec.com.br>
2022-06-05 14:46:15 -0300

    Projecao dos dados do Autor com DTO

---
+++ src/main/java/course/spring/mongodb/dto/AuthorDTO.java
@@ -0,0 +1,35 @@
+package course.spring.mongodb.dto;
+
+import course.spring.mongodb.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+    
+    private String id;
+    private String name;
+    
+    public AuthorDTO() {
+    }
+    
+    public AuthorDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+}
